66753cb62a678797fb2648d823ef89ee0dee34f4
YANO Satoru <satoru@yanonet.com>
2014-07-10 21:42:29 +0900

    appendモードの追加 schema指定ができるようにした

--- lib/csvql/csvql.rb
+++ lib/csvql/csvql.rb
@@ -13,45 +13,38 @@ module Csvql
       @db_file = if path && path.size > 0
                    path
                  elsif console
-                   @tmp_file = true
-                   Tempfile.new("csvql").path + ".sqlite3"
+                   @tmp_file = Tempfile.new("csvql").path
                  else
                    ":memory:"
                  end
       @db = SQLite3::Database.new(@db_file)
     end
 
-    def create_table(cols, header, table_name="tbl")
+    def create_table(cols, header, table_name="tbl", schema=nil)
       @col_size = cols.size
       @table_name = table_name
-      @col_name = if header
-                    cols
-                  else
-                    @col_size.times.map {|i| "c#{i}" }
-                  end
-      col = @col_name.map {|c| "#{c} NONE" }
-      sql = "CREATE TABLE #{@table_name} (#{col.join(",")})"
+      if schema
+        file = File.expand_path(schema)
+        col = if File.exist?(file)
+                File.open(file).read
+              else
+                schema
+              end
+        @col_name = col.split(",").map {|c| c.split.first.strip }
+      else
+        @col_name = if header
+                      cols
+                    else
+                      @col_size.times.map {|i| "c#{i}" }
+                    end
+        col = @col_name.map {|c| "#{c} NONE" }.join(",")
+      end
+      sql = "CREATE TABLE IF NOT EXISTS #{@table_name} (#{col})"
       @db.execute(sql)
     end
 
-    def single_insert(cols, line)
-      if cols.size != @col_size
-        puts "line #{line}: wrong number of fields in line"
-        return
-      end
-      col = cols.map {|c| "'#{c}'" }
-      sql = "INSERT INTO #{@table_name} VALUES(#{col.join(",")});"
-      @db.execute(sql)
-    end
-
-    def bulk_insert(bulk)
-      rows = []
-      bulk.each do |cols|
-        col = cols.map {|c| "'#{c}'" }
-        rows << "(#{col.join(',')})"
-      end
-      sql = "INSERT INTO #{@table_name} VALUES#{rows.join(",")};"
-      # puts sql
+    def drop_table(table_name="tbl")
+      sql = "DROP TABLE IF EXISTS #{table_name}"
       @db.execute(sql)
     end
 
@@ -62,7 +55,7 @@ module Csvql
 
     def insert(cols, line)
       if cols.size != @col_size
-        puts "line #{line}: wrong number of fields in line"
+        puts "line #{line}: wrong number of fields in line (skipping)"
         return
       end
       @pre ||= prepare(cols)
@@ -80,7 +73,7 @@ module Csvql
 
     def open_console
       system("sqlite3", @db_file)
-      File.delete(@db_file) if @tmp_file
+      File.delete(@tmp_file) if @tmp_file
     end
   end
 
@@ -97,17 +90,21 @@ module Csvql
       opt.on("--[no-]header",     "Treat file as having the first row as a header row") {|v| option[:header] = v }
       opt.on('--output-dlm="|"',  "Output delimiter (|)")                               {|v| option[:output_dlm] = v }
       opt.on("--save-to=FILE",    "If set, sqlite3 db is left on disk at this path")    {|v| option[:save_to] = v }
+      opt.on("--append",          "Append mode (not dropping any tables)")              {|v| option[:append] = v }
       opt.on("--skip-comment",    "Skip comment lines start with '#'")                  {|v| option[:skip_comment] = v }
       opt.on("--source=FILE",     "Source file to load, or defaults to stdin")          {|v| option[:source] = v }
       opt.on("--sql=SQL",         "SQL Command(s) to run on the data")                  {|v| option[:sql] = v }
       opt.on("--select=COLUMN",   "Select column (*)")                                  {|v| option[:select] = v }
+      opt.on("--schema=FILE or STRING", "Specify a table schema")                       {|v| option[:schema] = v }
       opt.on("--where=COND",      "Where clause")                                       {|v| option[:where] = v }
       opt.on("--table-name=NAME", "Override the default table name (tbl)")              {|v| option[:table_name] = v }
       opt.on("--verbose",         "Enable verbose logging")                             {|v| option[:verbose] = v }
       opt.parse!(argv)
 
       option[:source] ||= argv[0]
+      # option[:where] ||= argv[1]
       option[:table_name] ||= "tbl"
+      option[:output_dlm] ||= "|"
       option
     end
 
@@ -125,7 +122,7 @@ module Csvql
       csvfile = option[:source] ? File.open(option[:source]) : $stdin
 
       tbl = TableHandler.new(option[:save_to], option[:console])
-      bulk = []
+      tbl.drop_table(option[:table_name]) unless option[:append]
       tbl.exec("PRAGMA synchronous=OFF")
       tbl.exec("BEGIN TRANSACTION")
       csvfile.each.with_index(1) do |line,i|
@@ -134,22 +131,24 @@ module Csvql
         next if option[:skip_comment] && line.start_with?("#")
         row = line.parse_csv
         if i == 1
-          tbl.create_table(row, option[:header], option[:table_name])
+          tbl.create_table(row, option[:header], option[:table_name], option[:schema])
           next if option[:header]
         end
-        tbl.insert(row,i)
+        tbl.insert(row, i)
       end
       tbl.exec("COMMIT TRANSACTION")
 
-      if option[:select] || option[:where]
+      if option[:sql]
+        sql = option[:sql]
+      elsif option[:select] || option[:where]
         option[:select] ||= "*"
-        option[:sql] = "select #{option[:select]} from #{option[:table_name]}"
+        sql = "select #{option[:select]} from #{option[:table_name]}"
         if option[:where]
-          option[:sql] += " where #{option[:where]}"
+          sql += " where (#{option[:where]})"
         end
       end
 
-      tbl.exec(option[:sql], option[:output_dlm]||"|") if option[:sql]
+      tbl.exec(sql, option[:output_dlm]) if sql
       tbl.open_console if option[:console]
     end
   end
